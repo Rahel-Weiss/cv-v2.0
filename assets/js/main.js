ad4549a6d6da9696751e4362eb012e4753406899
Rahel Weiss <mail@rahelweiss.com>
2018-08-24 02:40:14 +0200

    late night coding

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,22 +21,45 @@ scrollLink.click(function(e) {
 window.onscroll = function() {
   stickyNav();
   balloonFire();
+  navBackground();
 };
+
+var $contentDivs = $(".section");
+var stickyOffset = $(".name-and-arrow").offset();
+
+function navBackground() {
+  $contentDivs.each(function(k) {
+    var _thisOffset = $(this).offset();
+    var _actPosition = _thisOffset.top - $(window).scrollTop();
+    if (
+      _actPosition < stickyOffset.top &&
+      _actPosition + $(this).height() > 0
+    ) {
+      $("#nav")
+        .removeClass("light dark")
+        .addClass($(this).hasClass("light") ? "light" : "dark");
+      return false;
+    }
+  });
+}
 
 const header = document.getElementById("nav");
 const arrow = document.getElementById("arrow");
-const balloons = document.getElementById("page-2");
+const balloons = document.getElementById("section-four");
+const start = document.getElementById("start");
 
-const sticky = header.offsetTop;
+const stickyOffSet = header.offsetTop;
 const balloonOffset = balloons.offsetTop;
 
 function stickyNav() {
-  if (window.pageYOffset > sticky) {
+  if (window.pageYOffset > stickyOffSet) {
     header.classList.add("sticky");
     arrow.classList.add("clear");
+    start.classList.add("sticky-start");
   } else {
     header.classList.remove("sticky");
     arrow.classList.remove("clear");
+    start.classList.remove("sticky-start");
   }
 }
 
---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require("gulp");
+
+gulp.task("default", ["root"]);
+
+gulp.task("root", function(cb) {
+  gulp.src("index.html").pipe(gulp.dest("build"));
+});
