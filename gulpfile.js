5bf042d0b991affb32ba341f661a76fad30b0328
Rahel Weiss <mail@rahelweiss.com>
2018-09-01 09:41:12 +0200

    gulp working, no javascript yet

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,67 @@
-// var gulp = require("gulp");
+"use strict";
 
-// gulp.task("default", ["root"]);
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const uglifycss = require("gulp-uglifycss");
+const concat = require("gulp-concat");
 
-// gulp.task("root", function(cb) {
-//   gulp.src("index.html").pipe(gulp.dest("build"));
+gulp.task("sass", function() {
+  return gulp
+    .src("assets/scss/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat("styles.css"))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("css", function() {
+  return gulp
+    .src("build/css/*.css")
+    .pipe(
+      uglifycss({
+        maxLineLen: 80,
+        uglyComments: true
+      })
+    )
+    .pipe(gulp.dest("build"));
+});
+
+// TO DO: CONCAT js
+gulp.task("script", function() {
+  return gulp
+    .src("assets/js/*.js")
+    .pipe(concat("script.min.js"))
+    .pipe(gulp.dest("build"));
+});
+
+// Compile and automatically prefix stylesheets
+// gulp.task("styles", () => {
+//   const AUTOPREFIXER_BROWSERS = [
+//     "ie >= 10",
+//     "ie_mob >= 10",
+//     "ff >= 30",
+//     "chrome >= 34",
+//     "safari >= 7",
+//     "opera >= 23",
+//     "ios >= 7",
+//     "android >= 4.4",
+//     "bb >= 10"
+//   ];
 // });
+
+gulp.task("style-sheets", gulp.series("sass", "css"));
+
+gulp.task("watch", function() {
+  gulp.watch("assets/scss/*.scss", ["sass"]);
+  gulp.watch("build/*.css", ["css"]);
+});
+
+const build = gulp.series("style-sheets", "script");
+
+// Run all the tasks
+gulp.task("default", function() {
+  gulp.watch("assets/scss/*.scss", gulp.series("style-sheets"));
+  // gulp.watch("build/*.css", gulp.series("css"));
+  gulp.watch("assets/js/*.js", gulp.series("script"));
+});
+
+gulp.task("build", build);
